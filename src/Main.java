4341853decc74352bfeb9a90653ae7857a1c6138
Himanshu Pawar <himanshupawarofficial2203.com>
2026-02-12 11:10:56 +0530

    V1

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number :");
+        int number = scanner.nextInt();
+        MultiThreadingDemo demo = new MultiThreadingDemo();
+        Thread thread1 = new Thread(demo);
+        Thread thread2 = new Thread(demo);
+        Thread thread3 = new Thread(demo);
+    }
+}
---
+++ src/MultiThreadingDemo.java
@@ -0,0 +1,15 @@
+public class MultiThreadingDemo implements Runnable{
+    private int number;
+    public MultiThreadingDemo() {
+        this.number = number;
+    }
+    @Override
+    public void run() {
+        for(int i = 1; i < 10; i++)
+        {
+            int result = number * i;
+            System.out.println(Thread.currentThread().getName() + " : " + number + " x " + i + " = " + result);
+        }
+
+    }
+}
